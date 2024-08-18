c6e3e33d92145a697751d929a4f6daed02de96fd
vathalasambasiva <vathalasambasiva0@gmail.com>
2024-08-18 19:24:42 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
 echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+echo 'into the local Maven repository.'
+
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+echo 'Extracting the project name and version from pom.xml.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 set +x
 
-echo 'The following command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
-set +x
+echo 'Running the Java application.'
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+if [ "$(uname)" == "Linux" ] || [ "$(uname)" == "Darwin" ]; then
+    # For Unix-based systems (Linux, macOS)
+    nohup java -jar target/${NAME}-${VERSION}.jar &
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    # For Windows systems
+    start java -jar target/${NAME}-${VERSION}.jar
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
